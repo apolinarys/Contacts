97f070bf1cb1602ae6c4daa40e44fbfd1eb5c4db
Polina <112948671+apolinarys@users.noreply.github.com>
2022-11-03 14:47:35 +0300

    Create cell

--- Contacts/Views/ContactsTableViewCell.swift
+++ Contacts/Views/ContactsTableViewCell.swift
@@ -10,15 +10,71 @@ import UIKit
 class ContactsTableViewCell: UITableViewCell {
     
     private lazy var nameLabel: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.medium)
+        label.text = "Name"
         return label
     }()
     
     private lazy var phoneLabel: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Phone"
         return label
     }()
+    
+    private lazy var skillsLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Skills"
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubviews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        nameLabel.text = ""
+        phoneLabel.text = ""
+        skillsLabel.text = ""
+    }
+    
+    func configure(contact: Contact) {
+        nameLabel.text = contact.name
+        phoneLabel.text = contact.phoneNumber
+        var skills = contact.skills[0]
+        for i in 1...skills.count - 1 {
+            skills += " * " + contact.skills[i]
+        }
+        skillsLabel.text = skills
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(phoneLabel)
+        contentView.addSubview(skillsLabel)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            
+            phoneLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            phoneLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            
+            skillsLabel.topAnchor.constraint(equalTo: phoneLabel.bottomAnchor, constant: 8),
+            skillsLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            skillsLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+    }
 }
